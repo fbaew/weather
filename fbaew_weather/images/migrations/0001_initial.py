755981f2e99d1ad3ea2dff8ae49d66c38934c2cd
Gregg Lewis <dev@gregglewis.net>
2015-08-01 06:41:00 +0000

    beginnings of web frontend...

---
+++ fbaew_weather/images/migrations/0001_initial.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ScrapedImage',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+    ]
