64b87985f17150a0baefd079e05fb25aaf92ba1d
Dylan Reviczky <drcodeservices@gmail.com>
2015-08-09 18:09:00 -0600

    Refactor dateRange as datetime-yielding generator. Refactor other methods to accomodate and update main example

--- ImageAggregator.py
+++ ImageAggregator.py
@@ -12,107 +12,100 @@ from os.path import isfile,join
 #Takes a datetime.utcnow()
 
 class ImageAggregator(object):
-    """Not instantiated directly. Contains logic that children will use/extend. 
+    """Not instantiated directly. Contains logic that children will use/extend.
 
         Attributes to be set by children:
         directoryPath -- string, local subfolder, for image storage
         url -- file root url for scraping, e.g 'https://weather.gc.ca/data/radar/temp_image/XSM/XSM_PRECIP_RAIN_'
         fileExtension -- short string. e.g. ".jpg" or ".GIF"
-        dateConverter -- Lambda function that takes "YYYY_MM_DD_HH_MM" and converts to string of url format.
+
+        Two methods:
+        timeToUrl(datetime) -- takes a datetime object, returns string of url format. Must be implemented by subclass
+        timeToString(datetime) - takes a datetime object and returns "YYYY_MM_DD_HH_MM" format
+
     """
-    def __init__(self, directoryPath, url, fileExtension, dateConverter):
-        #datetime object
-        self.created = datetime.datetime.utcnow()
+    def __init__(self, directoryPath, url, fileExtension, timeToUrl):
         #Strings
         self.directoryPath = directoryPath
         self.url = url
         self.fileExtension = fileExtension
-        #A function to turn local naming scheme string into child's URL one
-        self.dateConverter = dateConverter
 
-    def downloadImages(self, nameList):
-        """Takes list of filenames in format YYYY_MM_DD_HH_MM, downloads those images from self.url. 
+        self.timeToUrl = timeToUrl #implemented by subclass
 
-              Todo: Should accept single string arg. Split into separate functions.
+        self.timeToString = lambda dt: "{0:0>4}_{1:0>2}_{2:0>2}_{3:0>2}_{4:0>2}".format(dt.year,dt.month,dt.day,dt.hour,dt.minute)
+        """ Takes datetime object, Outputs string of format  "2015_07_29_17_40"
+
+            Output is the format of the radar url filenames and is being used as the local
+            storage naming scheme.
+        """
+
+    def downloadImages(self, datetimeList):
+        """Takes list of datetime objects to download, downloads those times from self.url.
         """
 
         urlList=[]
-        for dateString in nameList:
-            urlList.append(self.url + self.dateConverter(dateString) + self.fileExtension)
-        #for item,url in zip(urlList,nameList): print(item,url)
+        nameList=[]
+
+        for time in datetimeList:
+            urlList.append(self.url + self.timeToUrl(time) + self.fileExtension)
+            nameList.append(self.directoryPath + self.timeToString(time) + self.fileExtension)
+
         for fileName,url in zip(nameList,urlList):
-            fileName += self.fileExtension
-            if isfile(self.directoryPath + fileName):
+            if isfile(fileName):
                 print("File already exists, skipping: " + fileName)
-                continue #skips this file
+                continue
 
             try:
                 r = requests.get(url)
-                r.raise_for_status() 
-            except requests.exceptions.ConnectionError as e:
-                print("Skipping file. Connection error connecting to " + url)
+                r.raise_for_status()
+            except (requests.exceptions.HTTPError,
+                    requests.exceptions.ConnectionError) as e:
                 print(e)
-                print()
-                continue
-            except requests.exceptions.HTTPError as e:
-                print("Server served an HTTP error. Skipping " + url)
-                print(e)
-                print()
                 continue
             except requests.exceptions.RequestException as e:
                 print("Now would be a good time to handle a new exception.")
                 print(e)
-                print()
                 sys.exit(1)
 
             try:
-                imageFile = open(join(self.directoryPath, fileName),"wb")
+                imageFile = open(fileName,"wb")
                 print("Trying: " + url)
                 imageFile.write(r.content)
                 imageFile.close()
-                print("File written to: " + join(self.directoryPath + fileName) + "\n")
+                print("File written to: " + fileName + "\n")
             except IOError:
                 #make this better eventually
-                print("Error opening/writing  " + join(self.directoryPath, fileName) + ". Abort! Abort! Abandon program!")
+                print("Error opening/writing  " + fileName + ". Abort! Abort! Abandon program!")
                 raise
 
-    def dateRange(self, fromTime,toTime=datetime.datetime.utcnow(),minuteInterval=10):
-        """ Takes two datetime objects, returns sequential list of date strings in given minute increments 
 
-            Outputs list of format  "2015_07_29_17_40" 
-                                    (YYYY_MM_DD_HH_MM)
-            Output is the format of the radar url filenames and has been used as
-            the standard for internal representation. Children should provide a
-            dateConverter function that translates a string of this format to their URL format.
-
-            Needs to be tested with lists/corner cases, should implement case/handling 
-            for toTime < fromTime which gives a chronologically descending list.
-            Should make it iterable. 
+    def dateRange(self, fromTime, toTime=datetime.datetime.utcnow(), minuteInterval=10):
+        """ Generator function for datetime objects fromTime toTime at given minuteInterval.
         """
-
         #Floors times to 10 minutes, wiping seconds, microseconds
-        fromTime -= datetime.timedelta(minutes=fromTime.minute % 10, 
+        fromTime -= datetime.timedelta(minutes=fromTime.minute % 10,
                                         seconds=fromTime.second, microseconds=fromTime.microsecond)
-        toTime -= datetime.timedelta(minutes=toTime.minute % 10, 
+        toTime -= datetime.timedelta(minutes=toTime.minute % 10,
                                         seconds=toTime.second, microseconds=toTime.microsecond)
 
-        #Generate list of "2015_07_29_17_40" format strings
-
-        dateList = []
-        c = fromTime
-        while (c <= toTime):
-            dateList.append("{0:0>4}_{1:0>2}_{2:0>2}_{3:0>2}_{4:0>2}".format(c.year,c.month,c.day,c.hour,c.minute))
-            c += datetime.timedelta(minutes=minuteInterval)
-
-        print(dateList)
-        return dateList
+        if (fromTime <= toTime):
+            while (fromTime <= toTime):
+                yield fromTime
+                fromTime += datetime.timedelta(minutes=minuteInterval)
+        elif (fromTime > toTime):
+            while (fromTime >= toTime):
+                yield fromTime
+                fromTime -= datetime.timedelta(minutes=minuteInterval)
+        else:
+            print("AAAAAAHHHHHHH!")
+            raise StopIteration
 
     def makeLatestImage(self, fromTime):
         """ Needs implementation.
             Takes a datetime object 'from', generates a gif fromTime til present. use os calls to imagemagik? Put it in self.directoryPath"""
         pass
 
-    def printImageList(self): 
+    def printImageList(self):
         """Prints a list of files in the child's subdirectory"""
         files = [ f for f in listdir(self.directoryPath) if isfile(join(self.directoryPath,f)) ]
         if files == []:
@@ -122,13 +115,14 @@ class ImageAggregator(object):
         print("\nAll files in " + self.directoryPath)
         print(files)
         print("\n")
-        
-    def nameToTime(self, dateString):
+
+    def stringToTime(self, dateString):
         """Parses a date/time of format 2015_07_29_17_40 and returns a datetime object
 
             To add: error checking. Assumes proper format.
         """
-        obj = datetime.datetime(dateString.split("_"))
+        split = list(map(int,dateString.split("_")))
+        obj = datetime.datetime(split[0],split[1],split[2],split[3],split[4])
         return obj
 
 class Radar(ImageAggregator):
@@ -155,8 +149,8 @@ class Radar(ImageAggregator):
         self.fileExtension = ".GIF"
         #This service operates on the same naming scheme as the remote URLs,
         #no need to convert date string format. Passes string it receives.
-        self.dateConverter = lambda x: x
-        super().__init__(self.directoryPath,self.url,self.fileExtension,self.dateConverter)
+        self.timeToUrl = lambda x: self.timeToString(x)
+        super().__init__(self.directoryPath,self.url,self.fileExtension,self.timeToUrl)
 
 
 class Satellite(ImageAggregator):
@@ -171,23 +165,26 @@ class Satellite(ImageAggregator):
         self.directoryPath = "sat_img/"
         self.url = "http://weather.gc.ca/data/satellite/goes_wcan_1070_m_"
         self.fileExtension = ".jpg"
-        #Maybe shouldn't be a oneliner, but, for now... this turns '2015_07_29_17_40' into '2015@07@29_17h40m'
-        self.dateConverter = lambda string: "".join(map(lambda x,y: x + y, string.split('_'), ['@','@','_','h','m']))
-        
-        super().__init__(self.directoryPath,self.url,self.fileExtension,self.dateConverter)
+        #This should not be a oneliner, but, for now... this turns '2015_07_29_17_40' into '2015@07@29_17h40m'
+        self.timeToUrl = lambda time: "".join(map(lambda x,y: x + y, self.timeToString(time).split('_'), ['@','@','_','h','m']))
+
+        super().__init__(self.directoryPath,self.url,self.fileExtension,self.timeToUrl)
 
 
 if __name__ == "__main__":
     now = datetime.datetime.utcnow()
-    
+
     fortyMinutesAgo = now - datetime.timedelta(minutes=40)
-    
+
     sat = Satellite()
     sat.downloadImages(sat.dateRange(fortyMinutesAgo))
-    print(sat.dateConverter("2015_07_29_17_40"))
     sat.printImageList()
+
+    for date in sat.dateRange(now,fortyMinutesAgo):
+        print(date)
+    for date in sat.dateRange(fortyMinutesAgo,now):
+        print(date)
 
     rad = Radar()
     rad.downloadImages(rad.dateRange(fortyMinutesAgo))
-    print(rad.dateConverter("2015_07_29_17_40"))
     rad.printImageList()
