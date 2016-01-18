cf567041c1fb585320a25d8bed7b0a397ec45078
Gregg Lewis <code@gregglewis.net>
2016-01-18 00:10:24 -0500

    trying to compute timestamps for the last 10 available snaps

--- html/latest.js
+++ html/latest.js
@@ -3,3 +3,25 @@ var month;
 var day;
 var hour;
 var minute;
+
+var timestamps = [];
+
+function getLastTenDates(startingdate) {
+    var i = 10;
+    hours = startingdate.getHours()
+    minutes = startingdate.getMinutes()
+
+    while (i > 0) {
+        timestamps.push("" + hours + "" + minutes)
+        i -= 1
+        if (minutes >= 10) {
+            minutes -= 10
+        }
+        else {
+            minutes = 50
+            if (hours > 0) {
+                hours -= 1
+            }
+        }
+    }
+}
